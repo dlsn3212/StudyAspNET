e93fbf573c3eddcac841cb3a3af0570239b5a38c
yujin <dywjd2341@naver.com>
2020-08-11 13:44:15 +0900

    Asp.net

---
+++ EddyHomePageSolution/EddyNewHome/Controllers/EddyHomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EddyNewHome.Controllers
+{
+    public class EddyHomeController : Controller
+    {
+        // GET: EddyHome
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
